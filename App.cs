642865281a44706e243fc2a604a0b08fb05a3381
User344 <user344u@gmail.com>
2024-05-02 15:41:35 +0300

    first commit

---
+++ App.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using LeagueProxyLib;
+
+var leagueProxy = new LeagueProxy();
+
+leagueProxy.Events.OnProcessConfigPublic += (string content) =>
+{
+    var configObject = JsonSerializer.Deserialize<JsonNode>(content);
+
+    // disable vanguard
+    if (configObject?["anticheat.vanguard.enabled"] is not null)
+    {
+        Console.WriteLine("patched anticheat.vanguard.enabled");
+        configObject["anticheat.vanguard.enabled"] = false;
+    }
+
+    if (configObject?["lol.client_settings.vanguard.enabled"] is not null)
+    {
+        Console.WriteLine("patched lol.client_settings.vanguard.enabled");
+        configObject["lol.client_settings.vanguard.enabled"] = false;
+    }
+
+    var lol = configObject?["keystone.products.league_of_legends.patchlines.live"];
+    if (lol is not null)
+    {
+        var configs = lol["platforms"]?["win"]?["configurations"]?.AsArray();
+        if (configs is not null)
+        {
+            for (var i = 0; i < configs.Count; ++i)
+            {
+                var dependencies = configs[i]!["dependencies"]!.AsArray();
+
+                var vanguard = dependencies.FirstOrDefault(x => x!["id"]!.GetValue<string>() == "vanguard");
+                if (vanguard is not null)
+                {
+                    Console.WriteLine("removing vanguard dependency");
+                    dependencies.Remove(vanguard);
+                }
+            }
+        }
+    }
+
+    return JsonSerializer.Serialize(configObject);
+};
+
+var process = leagueProxy.StartAndLaunchRCS();
+if (process is null)
+{
+    Console.WriteLine("Failed to create RCS process!");
+    leagueProxy.Stop();
+    return;
+}
+
+await process.WaitForExitAsync();
+
+// Gracefully exit.
+leagueProxy.Stop();
---
+++ README.md
@@ -0,0 +1,12 @@
+# LeagueNoVGK
+Simple tool to make Riot Client think that Vanguard is disabled and League of Legends doesnt not need it, to be able to launch LCU with Vanguard disabled/uninstalled.
+
+> [!CAUTION]
+> This is **NOT** a Vanguard bypass. This will **NOT** let you play League of Legends without Vanguard. It only lets you launch LCU without it.
+
+# Usage
+1. Close Riot Client.
+
+2. Launch the app.
+
+Thats it.
